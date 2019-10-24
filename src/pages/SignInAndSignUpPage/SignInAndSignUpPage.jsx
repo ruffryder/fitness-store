1a9357b71475e00033bff05be18536c50b614084
Christian Dimitrov <christian_dimitrov@abv.bg>
2019-10-24 14:23:22 +0300

    change homepage prop in package.json

--- src/pages/SignInAndSignUpPage/SignInAndSignUpPage.jsx
+++ src/pages/SignInAndSignUpPage/SignInAndSignUpPage.jsx
@@ -6,9 +6,6 @@ import "./SignInAndSignUpPage.scss";
 function SignInAndSignUpPage() {
   return (
     <>
-      <h1 className="sign-in-and-sign-up-title">
-        Sign in into your account or create a new one
-      </h1>
       <div className="sign-in-and-sign-up">
         <SignIn />
         <SignUp />
