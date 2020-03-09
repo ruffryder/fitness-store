39368806ffb92ef84844fbeee94d87b8149f43e2
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-03-09 16:45:29 +0200

    changing titles in CollectionPreview component to Links that lead to corresponding category

--- src/components/CollectionPreview/CollectionPreview.jsx
+++ src/components/CollectionPreview/CollectionPreview.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./CollectionPreview.scss";
 import CollectionItem from "../CollectionItem/CollectionItem";
+import { Link } from "react-router-dom";
 
 function CollectionPreview({ title, items }) {
   return (
     <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <Link to={`shop/${title.toLowerCase()}`}>
+        <h1 className="title">{title.toUpperCase()}</h1>
+      </Link>
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
           .map(item => {
+            console.log(item);
             return <CollectionItem key={item.id} item={item} />;
           })}
       </div>
